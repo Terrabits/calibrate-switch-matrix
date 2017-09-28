5df22d65a94a5cdc59e1ea41a1cdc50796ef5cc7
Nick Lalic <nick.lalic@gmail.com>
2017-09-27 17:50:46 -0700

    Tested lib.cli.vna.init, open_set

--- python/test/test_cli.py
+++ python/test/test_cli.py
@@ -2,12 +2,20 @@ import lib.cli               as cli
 from   test.helpers.args import MockArgs
 from   ddt               import ddt, data
 from   pathlib           import Path
+from   rohdeschwarz.instruments.vna import Vna
 import unittest
 
+valid_address   = 'rsa22471.local'
+invalid_address = '1.2.3.4'
+
 fixture_path    = Path(__file__).parent.parent.parent / 'test' / 'fixtures'
+
 procedures_path = fixture_path    / 'procedures'
 invalid_yaml    = procedures_path / 'invalid.yaml'
 valid_proc      = procedures_path / 'procedure.yaml'
+
+set_file        = fixture_path    / 'sets' / 'set.zvx' # .zvx | .znb added by Vna
+text_file       = valid_proc
 
 @ddt
 class TestCli(unittest.TestCase):
@@ -18,21 +26,19 @@ class TestCli(unittest.TestCase):
         cls.invalid_yaml = cls.procedures / 'invalid.yaml'
         cls.valid_proc   = cls.procedures / 'procedure.yaml'
 
-    @data({'address': 'rsa22471.local', 'is vna': True },
-          {'address': '1.2.3.4',        'is vna': False},
-          {'address': None,             'is vna': False})
+    @data({'address': valid_address,   'is vna': True },
+          {'address': invalid_address, 'is vna': False},
+          {'address': None,            'is vna': False})
     def test_is_vna(self, data):
         args = MockArgs()
-        args.is_vna      = True
         args.vna_address = data['address']
         self.assertEqual(cli.vna.is_vna(args), data['is vna'])
 
-    @data({'address': 'rsa22471.local', 'is matrix': True },
-          {'address': '1.2.3.4',        'is matrix': False},
-          {'address': None,             'is matrix': False})
+    @data({'address': valid_address,   'is matrix': True },
+          {'address': invalid_address, 'is matrix': False},
+          {'address': None,            'is matrix': False})
     def test_is_matrix(self, data):
         args = MockArgs()
-        args.is_matrix      = True
         args.matrix_address = data['address']
         self.assertEqual(cli.matrix.is_matrix(args), data['is matrix'])
 
@@ -42,6 +48,17 @@ class TestCli(unittest.TestCase):
         args = MockArgs()
         args.procedure_filename = data['filename']
         self.assertEqual(cli.procedure.is_procedure(args), data['is procedure'])
+
+    @data({'filename': str(set_file),  'result': True},
+          {'filename': None,           'result': None},
+          {'filename': str(text_file), 'result': False})
+    def test_init_vna(self, data):
+        filename = data['filename']
+        success  = data['result']
+        vna = Vna()
+        vna.open_tcp(valid_address)
+        self.assertTrue(vna.id_string())
+        self.assertEqual(cli.vna.init(vna, filename), success)
 
 if __name__ == '__main__':
     unittest.main()
