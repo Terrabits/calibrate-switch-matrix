434c6141b0edb235b7d6f53e788df889fbeb4ef5
Nick Lalic <nick.lalic@gmail.com>
2017-10-02 22:35:14 -0700

    wip: python vna/cal unit calibration pairs

--- python/lib/calibration.py
+++ python/lib/calibration.py
@@ -1,3 +1,11 @@
+import pdb
+
+def make_step(vnaPorts):
+    result = []
+    for i in range(0, len(vnaPorts)):
+        result.append([vnaPorts[i], i+1])
+    return result
+
 def create_steps(ports, cal_unit_ports):
     if cal_unit_ports <= 1:
         return []
@@ -7,20 +15,20 @@ def create_steps(ports, cal_unit_ports):
     ports = ports.copy()
 
     if len(ports) == 1:
-        return [ports]
+        return [[[*ports, 1]]]
 
     ports.sort()
     common_port = ports.pop(0)
     steps = []
     while ports:
         if len(ports) > cal_unit_ports - 1:
-            step = ports[0 : cal_unit_ports-1]
-            step.insert(0, common_port)
-            steps.append(step)
+            vnaPorts = ports[0 : cal_unit_ports-1]
+            vnaPorts.insert(0, common_port)
+            steps.append(make_step(vnaPorts))
             ports = ports[cal_unit_ports-1:]
         else:
-            step = ports.copy()
-            step.insert(0, common_port)
-            steps.append(step)
+            vnaPorts = ports.copy()
+            vnaPorts.insert(0, common_port)
+            steps.append(make_step(vnaPorts))
             ports.clear()
     return steps
--- python/test/test_calibration.py
+++ python/test/test_calibration.py
@@ -4,16 +4,16 @@ import unittest
 
 @ddt
 class TestCalibration(unittest.TestCase):
-    @data({'ports': [],              'cal unit size': 2, 'steps':[                         ]},
-          {'ports': [1,2,3,4],       'cal unit size': 0, 'steps':[                         ]},
-          {'ports': [1],             'cal unit size': 2, 'steps':[ [1]                     ]},
-          {'ports': [1,2],           'cal unit size': 2, 'steps':[ [1,2]                   ]},
-          {'ports': [1,2,3],         'cal unit size': 2, 'steps':[ [1,2], [1,3]                 ]},
-          {'ports': [1,2,3,4],       'cal unit size': 2, 'steps':[ [1,2], [1,3], [1,4] ]},
-          {'ports': [1,2,3,4],       'cal unit size': 3, 'steps':[ [1,2,3],      [1,4] ]},
-          {'ports': [1,2,3,4],       'cal unit size': 4, 'steps':[ [1,2,3,4]               ]},
-          {'ports': [1,2,3,4],       'cal unit size': 5, 'steps':[ [1,2,3,4]               ]},
-          {'ports': [1,2,3,4,5,6,7], 'cal unit size': 4, 'steps':[ [1,2,3,4], [1,5,6,7]    ]})
+    @data({'ports': [],              'cal unit size': 2, 'steps':[ ]},
+          {'ports': [1,2,3,4],       'cal unit size': 0, 'steps':[ ]},
+          {'ports': [1],             'cal unit size': 2, 'steps':[ [[1,1]] ]},
+          {'ports': [1,2],           'cal unit size': 2, 'steps':[ [[1,1],[2,2]] ]},
+          {'ports': [1,2,3],         'cal unit size': 2, 'steps':[ [[1,1],[2,2]], [[1,1],[3,2]] ]},
+          {'ports': [1,2,3,4],       'cal unit size': 2, 'steps':[ [[1,1],[2,2]], [[1,1],[3,2]], [[1,1],[4,2]] ]},
+          {'ports': [1,2,3,4],       'cal unit size': 3, 'steps':[ [[1,1],[2,2],[3,3]], [[1,1],[4,2]] ]},
+          {'ports': [1,2,3,4],       'cal unit size': 4, 'steps':[ [[1,1],[2,2],[3,3],[4,4]] ]},
+          {'ports': [1,2,3,4],       'cal unit size': 5, 'steps':[ [[1,1],[2,2],[3,3],[4,4]] ]},
+          {'ports': [1,2,3,4,5,6,7], 'cal unit size': 4, 'steps':[ [[1,1],[2,2],[3,3],[4,4]], [[1,1],[5,2],[6,3],[7,4]] ]})
     def test_create_steps(self, data):
         steps = create_steps(data['ports'], data['cal unit size'])
         self.assertEqual(steps, data['steps'])
